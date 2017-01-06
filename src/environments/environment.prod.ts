3cfac3d600af092b228a71e6fc98e8e7c3aa25b4
Olivier Ailloud <o.ailloud@gmail.com>
2017-01-06 12:13:38 +0100

    update the production configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  // 817955 being hashbangfr group id
+  GITLAB_API_BASE_URL: 'https://gitlab.com/api/v3/groups/817955/',
+  // private token for user kanban
+  GITLAB_API_TOKEN: 'JbdkpAL4A1EJUf2DMLTD',
 };
