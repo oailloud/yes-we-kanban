45f36c3555baba96dd140a7b38b90d8844f149a7
Olivier Ailloud <o.ailloud@gmail.com>
2017-02-21 10:58:51 +0100

    update obsolete configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,11 @@
 
 export const environment = {
   production: false,
-  // 817955 being hashbangfr group id
-  GITLAB_API_BASE_URL: 'https://gitlab.com/api/v3/groups/817955/',
-  // private token for user kanban
-  GITLAB_API_TOKEN: 'JbdkpAL4A1EJUf2DMLTD',
+
+  GITLAB_API_BASE_URL: 'https://gitlab.com/api/v3/groups/xxxx/',
+  GITLAB_API_TOKEN: 'XXXX',
   projectsColorMap: {
-      komaks: '#ff995d',
-      getmypool: '#bfcdff',
-      owltail: '#bfe8b7',
-      owlface: '#e2f1d3',
-      owlright: '#74da6d',
-      hashbang: '#f79eec',
-      mngdeps: '#f1ff7a',
-      hosting: '#d6d6d6',
-      lepinard: '#dc4d66',
-      maisondesaveugles: '#56564a',
+      projet1: '#ff995d',
+      projet2: '#bfcdff',
   },
 };
