b67fce6d0cc00a55ce6980d369f0309e25dc2783
Olivier Ailloud <o.ailloud@gmail.com>
2017-02-28 14:33:36 +0100

    setup a working example for ywk

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,12 @@
 
 export const environment = {
   production: false,
-
-  GITLAB_API_BASE_URL: 'https://gitlab.com/api/v3/groups/xxxx/',
-  GITLAB_API_TOKEN: 'XXXX',
+  // 817955 being hashbangfr group id
+  GITLAB_API_BASE_URL: 'https://gitlab.com/api/v3/groups/1370476/',
+  // private token for user kanban
+  GITLAB_API_TOKEN: '8-gDLrcSd-Vz-zyLtYYk',
   projectsColorMap: {
-      projet1: '#ff995d',
-      projet2: '#bfcdff',
+      'ywk-ex1': '#ff995d',
+      'ywk-ex2': '#bfcdff',
   },
 };
